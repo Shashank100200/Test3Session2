1839933c9a48f0d6416b16dfb20602311375d159
Shashank B N <97089480+Shashank100200@users.noreply.github.com>
2022-03-03 09:08:26 +0000

    7th program

--- p7final.c
+++ p7final.c
@@ -1,1 +1,32 @@
-
+#include<stdio.h>
+struct _fraction
+{
+int num,den;
+}
+typedef Fraction;
+int gcd(int a,int b)
+{
+  int gcd(int a,int b);
+  int t;
+    while (b !=0)
+      {
+        t = b;
+        b = a%b;
+        a = t;
+        }
+    return a;
+}
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("Input a fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+int main()
+{
+  Fraction f;
+  f=input_fraction();
+  printf("The fraction is %d/%d\n",f.num,f.den);
+  return 0;
+}
